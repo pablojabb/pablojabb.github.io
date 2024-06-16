a18b31fca3921469fac9d9031717050127687392
A.Jumaya <142668163+pablojabb@users.noreply.github.com>
2024-06-16 19:11:11 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+var icon = document.getElementById("icontheme");
+var text = document.getElementById("themetext");
+
+icon.onclick  = function () {
+  const logo = document.querySelector(".golo");
+  logo.classList.toggle("dark");
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    icon.src = "./assets/lightmode.svg";
+  } else {
+    icon.src = "./assets/darkmode.svg";
+  }
+};
+
+text.onclick  = function () {
+  const logos = document.querySelector(".golos");
+  logos.classList.toggle("dark");
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    icon.src = "./assets/lightmode.svg";
+  } else {
+    icon.src = "./assets/darkmode.svg";
+  }
+};
